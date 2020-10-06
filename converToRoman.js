d46314c478ae5b39ace7f8016b2c8aad45eb744e
Alex Parker <alexpparker@gmail.com>
2020-10-06 22:58:17 +0100

    adding roman numeral converter

---
+++ converToRoman.js
@@ -0,0 +1,17 @@
+function convertToRoman(num) {
+ let str = '';
+ if (num>=1000) {str+="M"; num-=1000; str+=convertToRoman(num)} else
+ if (num>=900) {str += "CM"; num-=900; str+=convertToRoman(num)} else
+ if (num>=500) {str+= "D"; num-=500; str+=convertToRoman(num)} else
+ if (num>=400) {str+="CD"; num-=400; str+=convertToRoman(num)} else
+ if (num>=100) {str+="C"; num-=100; str+=convertToRoman(num)} else
+ if (num>=90) {str+="XC"; num-=90; str+=convertToRoman(num)} else
+ if (num>=50) {str+="L"; num-=50; str+=convertToRoman(num)} else
+ if (num>=40) {str+="XL"; num-=40; str+=convertToRoman(num)} else
+ if (num>=10) {str+="X"; num-=10; str+=convertToRoman(num)} else
+ if (num>=9) {str+="IX"; num-=9; str+=convertToRoman(num)} else
+ if (num>=5) {str+="V"; num-=5; str+=convertToRoman(num)} else
+ if (num>=4) {str+="IV"; num-=4; str+=convertToRoman(num)} else
+ if (num>=1) {str+="I"; num-=1; str+=convertToRoman(num)}
+ return str;
+}
